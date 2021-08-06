f57f620910230990f83d4bd991b973da4b53a6af
Crowdbotics <team@crowdbotics.com>
2021-08-06 15:05:54 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "quiet_poetry_29566Identifier",
+  url: "https://quiet_poetry_29566Identifier.botics.co",
+  api: "https://quiet_poetry_29566Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
